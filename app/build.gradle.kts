c7ccf22aaedb138f8ea97c7a09dc24a2afd952e8
huiche114514 <L2166981978@163.com>
2025-01-05 12:58:49 +0800

    初步适配横屏ui

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         minSdk = 24
         //noinspection OldTargetApi
         targetSdk = 34
-        versionCode = 115
-        versionName = "1.15"
+        versionCode = 116
+        versionName = "1.16"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
