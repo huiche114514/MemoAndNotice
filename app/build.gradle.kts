05b404949f9bf7927837d2d486bd88d1905c6d88
Yukiho <L2166981978@163.com>
2025-03-06 21:07:53 +0800

    修改部分界面

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,9 +34,8 @@ android {
 }
 
 dependencies {
-
     implementation(libs.appcompat)
-    implementation(libs.material.v1120)
+    implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
     testImplementation(libs.junit)
